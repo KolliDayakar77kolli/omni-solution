9103eef6666a38b4a31af2c554f1343911830159
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2023-08-26 19:30:30 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdfTUVDuHuQyrUKxB8VMwmSh8929J6OZc",
+  authDomain: "crypto-hunter-b5bdb.firebaseapp.com",
+  projectId: "crypto-hunter-b5bdb",
+  storageBucket: "crypto-hunter-b5bdb.appspot.com",
+  messagingSenderId: "931858804636",
+  appId: "1:931858804636:web:0c2c116a3147d86c0f6259"
+};
+export default firebaseConfig
